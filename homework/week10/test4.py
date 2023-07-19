d09544020ae2af2fab2dcd5d5bcff84eb71a810f
codezzzsleep <codezzzsleep@163.com>
2023-07-19 15:55:55 +0800

    测试 nn.MaxPool2d 模块

---
+++ homework/week10/test4.py
@@ -0,0 +1,13 @@
+import torch
+
+input = [3, 4, 6, 5, 7,
+         2, 4, 6, 8, 2,
+         1, 6, 7, 8, 4,
+         9, 7, 4, 6, 2,
+         3, 7, 5, 4, 1]
+input = torch.Tensor(input).view(1, 1, 5, 5)
+
+pooling = torch.nn.MaxPool2d(kernel_size=2)
+
+output = pooling(input)
+print(output)
